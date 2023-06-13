38f5da7f81b5f017f3b4bac313b66f6ae5d5ed5f
Arvin Sultan Satria <arvinsultansatria@gmail.com>
2023-06-14 00:43:04 +0700

    Menambahkan Tugas10

--- PWeb2023-Tugas10-I-2200018418-Arvin Sultan Satria.php
+++ PWeb2023-Tugas10-I-2200018418-Arvin Sultan Satria.php
@@ -1,34 +1,35 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nilai = $_POST['nilai'];
+    echo "<i>Tugas 10.2.1 (konversi nilai angka ke huruf)</i><br><br>";
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $nilai = $_POST['nilai'];
 
-    if ($nilai >= 80 ) {
-        $keterangan = "A";
-    } elseif ($nilai >= 76.25 && $nilai < 79.99) {
-        $keterangan = "A-";
-    } elseif ($nilai >= 68.75 && $nilai < 76.24) {
-        $keterangan = "B+";
-    } elseif ($nilai >= 65 && $nilai < 68.74) {
-        $keterangan = "B";
-    } elseif ($nilai >= 62.50 && $nilai < 64.99) {
-        $keterangan = "B-";
-    } elseif ($nilai >= 57.50 && $nilai < 62.49) {
-        $keterangan = "C+";
-    } elseif ($nilai >= 55 && $nilai < 57.49) {
-        $keterangan = "C";
-    } elseif ($nilai >= 51.25 && $nilai < 54.99) {
-        $keterangan = "C-";
-    } elseif ($nilai >= 43.75 && $nilai < 51.24) {
-        $keterangan = "D+";
-    } elseif ($nilai >= 40 && $nilai < 43.74) {
-        $keterangan = "D";
-    } elseif ($nilai >= 0 && $nilai < 39.99) {
-        $keterangan = "E";
+        if ($nilai >= 80 ) {
+            $keterangan = "A";
+        } elseif ($nilai >= 76.25 && $nilai < 79.99) {
+            $keterangan = "A-";
+        } elseif ($nilai >= 68.75 && $nilai < 76.24) {
+            $keterangan = "B+";
+        } elseif ($nilai >= 65 && $nilai < 68.74) {
+            $keterangan = "B";
+        } elseif ($nilai >= 62.50 && $nilai < 64.99) {
+            $keterangan = "B-";
+        } elseif ($nilai >= 57.50 && $nilai < 62.49) {
+            $keterangan = "C+";
+        } elseif ($nilai >= 55 && $nilai < 57.49) {
+            $keterangan = "C";
+        } elseif ($nilai >= 51.25 && $nilai < 54.99) {
+            $keterangan = "C-";
+        } elseif ($nilai >= 43.75 && $nilai < 51.24) {
+            $keterangan = "D+";
+        } elseif ($nilai >= 40 && $nilai < 43.74) {
+            $keterangan = "D";
+        } elseif ($nilai >= 0 && $nilai < 39.99) {
+            $keterangan = "E";
+        }
+    } else {
+        $nilai = "";
+        $keterangan = "";
     }
-} else {
-    $nilai = "";
-    $keterangan = "";
-}
 ?>
 
 <!DOCTYPE html>
@@ -49,6 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <br><br>
 
 <?php
+    echo "<i>Tugas 10.2.2 (konversi skrip Javascript ke PHP)</i><br><br>";
     $baris = $i = $j = 0;
     $nilai = $tinggi = 5;
     $tinggi = intval($nilai);
